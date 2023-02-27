77e3f82bbf4861f5c0c2cc9832ad226932ae4d82
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2023-02-27 13:30:29 -0800

    pushing view changes

--- My Book Shelf/ContentView.swift
+++ My Book Shelf/ContentView.swift
@@ -15,9 +15,18 @@ struct Restaurant: Identifiable {
 
 struct RestaurantRow: View {
     var restaurant: Restaurant
-
+    
     var body: some View {
         Text("Come and eat at \(restaurant.name)")
+    }
+}
+
+struct ColorSquare: View {
+    let color: Color
+    
+    var body: some View {
+        color
+        .frame(width: 150, height: 150)
     }
 }
 
@@ -28,20 +37,79 @@ struct ContentView: View {
         Restaurant(name: "Original Joe's")
     ]
     
+    let gradient = LinearGradient(colors: [Color.orange,Color.green],
+                                  startPoint: .top, endPoint: .bottom)
+    
+    let columns = [
+        GridItem(.fixed(100)),
+               GridItem(.flexible()),
+        ]
+    
     var body: some View {
-        NavigationView{
-            ZStack{
-                Color.gray
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .opacity(0.8)
+                    .ignoresSafeArea()
                 
-                VStack{
-                    List(restaurants) { restaurant in
-                        RestaurantRow(restaurant: restaurant)
+                ScrollView(showsIndicators: false) {
+                    
+                    
+                    Grid {
+                        GridRow {
+                            ForEach(0..<3) { _ in
+
+                                ColorSquare(color: .pink)
+                            }
+                        }
+                        GridRow {
+                            ForEach(0..<3) { _ in
+
+                                ColorSquare(color: .yellow)
+                            }
+                        }
+                        GridRow {
+                            ForEach(0..<3) { _ in
+
+                                ColorSquare(color: .mint)
+                            }
+                        }
                     }
+                    
+//                    LazyVGrid(columns: columns, spacing: 20) {
+//
+//                        HStack{
+//
+//
+//                            ForEach(restaurants) { restaurant in
+//
+//                                ZStack {
+//                                    RoundedRectangle(cornerRadius: 5)
+//
+//                                    VStack {
+//                                        Text(restaurant.name)
+//                                            .foregroundColor(.black)
+//
+//                                        Text("card.answer")
+//                                            .foregroundColor(.gray)
+//                                    }
+//                                    .padding(10)
+//                                    .multilineTextAlignment(.center)
+//                                }
+//                                .frame(width: 150, height: 150)
+//                            }
+//                            .shadow(radius: 10)
+//                        }
+//                    }
                 }
+                
+                    
+                
+                
+                .navigationTitle("Bookshelf")
+                .font(.title2)
+                .foregroundColor(.white)
             }
-//            .ignoresSafeArea()
-            
-            .navigationTitle("Bookshelf")
         }
     }
 }
